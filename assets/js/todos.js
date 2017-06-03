3d3a430eb1aeac0d7993a2d40ecb1acff1ded292
Dylan Thomson <djthomson88@gmail.com>
2017-06-02 20:19:34 -0400

    Remove Todos
    -Add event that handles removing a todo when the X button is clicked

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,13 @@
 $("li").click(function() {
 	$(this).toggleClass("completed");
 });
+
+// Click on X to delete Todo
+$("span").click(function(event) {
+	//remove li
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove;
+	});
+	//stop event from bubbling up to parent elements
+	event.stopPropagation();
+});
